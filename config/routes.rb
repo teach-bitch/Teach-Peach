e91c4dc1e95b6d536e77bc91605d0fa95880ed72
Octopussyzz <spudd.otw@gmail.com>
2019-03-12 17:26:33 +0100

    stripe is plugged

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root 'articles#index'
   resources :categories
   resources :articles
+  resources :charges
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
