c784f7690c628a20ffa1d209589284d2a7944fb0
smelldat <valentin.cluzan33@gmail.com>
2019-03-12 17:37:45 +0100

    create seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,63 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# =========== DESTROY ===========
+
+puts 'Purge de la DB en cours...'
+
+User.destroy_all
+Category.destroy_all
+Article.destroy_all
+ArticleCategory.destroy_all
+
+
+
+ActiveRecord::Base.connection.reset_pk_sequence!('users')
+ActiveRecord::Base.connection.reset_pk_sequence!('articles')
+ActiveRecord::Base.connection.reset_pk_sequence!('categories')
+ActiveRecord::Base.connection.reset_pk_sequence!('article_categories')
+puts 'Purge terminée.'
+sleep(1)
+
+# =========== CREATION ===========
+
+10.times do
+  user = User.create!(
+    email: Faker::Internet.unique.email,
+    password: Faker::Internet.password,
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.first_name,
+    username: Faker::DcComics.unique.villain,
+    birthdate: Faker::Date.birthday(9, 156))
+  print '.'
+end
+puts '=== User DB created ==='
+
+
+5.times do
+  category = Category.create!(
+    title: Faker::Construction.heavy_equipment)
+  print '.' 
+end
+puts '=== Category DB created ==='
+
+20.times do
+  article = Article.create!(
+  	user_id: User.all.sample.id,
+    title: Faker::Food.fruits,
+    for_adult: Faker::Boolean.boolean,
+    content: Faker::GreekPhilosophers.quote)
+  print '.'
+end
+puts '=== Article DB created ==='
+
+
+
+50.times do
+  article_categorie = ArticleCategory.create!(
+    category_id: Category.all.sample.id,
+    article_id: Product.all.sample.id)
+  print '.'
+end
+puts '=== ArticleCategory DB created ==='
