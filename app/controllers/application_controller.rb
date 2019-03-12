b97b4f4c1831af05d259f5c3bd7026753d437737
TheWalkingNev <alexandre.gasior@gmail.com>
2019-03-12 19:35:59 +0100

    additional column role added to permitted parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :username, :birthdate])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :username, :birthdate])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :username, :birthdate, :role])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :username, :birthdate, :role])
   end
 end
