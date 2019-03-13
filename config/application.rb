481e372d57146d4ca6bd546a169ace2140394969
kokogakayui <kokogakayui@gmail.com>
2019-03-13 10:12:43 +0100

    traduction fr devise ok

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,8 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+I18n.locale = :fr
+
 module TeachPeach
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
