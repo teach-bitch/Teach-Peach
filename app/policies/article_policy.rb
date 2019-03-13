84f3ea7120b448cedfe7516b12490027040905ba
kokogakayui <kokogakayui@gmail.com>
2019-03-13 16:40:53 +0100

    fix show article policy

--- app/policies/article_policy.rb
+++ app/policies/article_policy.rb
@@ -1,6 +1,5 @@
 class ArticlePolicy < ApplicationPolicy
-  # our authorization rules will go here
-    attr_reader :user, :article
+  attr_reader :user, :article
 
   def initialize(user, article)
     @user = user
@@ -8,45 +7,34 @@ class ArticlePolicy < ApplicationPolicy
   end
 
   def index?
-    
   end
 
   def show?
-    puts "in show?" * 30
-    
-    def show?
-
     if article.for_adult == true
-      user.role != "user_minor" 
+      user.role != "user_minor"
     else
       user.role == "user_minor" || user.role == 'user' || user.role == 'admin' || article.user_id == user.id || user.role == "subscriber"
     end
-
   end
 
-  end
-
-  def new?  
-    user.role == 'admin' 
+  def new?
+    user.role == 'admin'
   end
 
   def edit?
-    user.role == 'admin' 
+    user.role == 'admin'
   end
 
   def create?
-    user.role == 'admin' 
-    
+    user.role == 'admin'
   end
 
   def update?
-    user.role == 'admin' 
-    
+    user.role == 'admin'
   end
 
   def destroy?
     user.role == 'admin' || article.user_id == user.id
   end
-
 
 end
