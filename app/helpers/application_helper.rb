40af5d5521afc29c9b8d2f57562b064e6be59b06
kokogakayui <kokogakayui@gmail.com>
2019-03-13 01:14:26 +0100

    add icons to alerts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,13 @@ module ApplicationHelper
       when 'alert' then "alert-warning"
     end
   end
+
+  def flash_icons(type)
+    case type
+      when 'notice' then "travel_info"
+      when 'success' then "ui-2_like"
+      when 'error' then "objects_support-17"
+      when 'alert' then "ui-1_bell-53"
+    end
+  end
 end
