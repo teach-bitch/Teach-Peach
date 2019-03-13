b441c2cb896be1bc7058b4af034bcf2f0efa9a83
willyspa <armed95@hotmail.fr>
2019-03-13 19:40:00 +0100

    add routes admin namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    resources :categories
+    resources :articles
+    resources :users 
+  end
   root 'articles#index'
   resources :categories
   resources :articles
