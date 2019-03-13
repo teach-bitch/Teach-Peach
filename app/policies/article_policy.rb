e62f7753cc99e7e09539d012f7fd308754b0ead4
Yacinth <fevriercjjc@gmail.com>
2019-03-13 10:13:01 +0100

    add other policy method of article_policy

--- app/policies/article_policy.rb
+++ app/policies/article_policy.rb
@@ -7,7 +7,42 @@ class ArticlePolicy < ApplicationPolicy
     @article = article
   end
 
+  def index
+    
+  end
+
+  def show
+    user.role == 'admin'
+
+    if article.for_adult == true
+        user.role != "user_minor"
+    else
+        article.user_id == user.id || user.role == "subscriber"
+    end
+
+  end
+
+  def new    
+    user.role == 'admin' 
+  end
+
+  def edit
+    user.role == 'admin' 
+  end
+
+  def create
+    user.role == 'admin' 
+    
+  end
+
+  def update
+    user.role == 'admin' 
+    
+  end
+
   def destroy?
     user.role == 'admin' || article.user_id == user.id
   end
+
+
 end
