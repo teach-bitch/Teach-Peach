02aeeda0662c5e945eddbbb454e392b2d7abca64
TheWalkingNev <alexandre.gasior@gmail.com>
2019-03-14 18:27:46 +0100

    changed the informations to send to users

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,13 +2,8 @@ class UserMailer < ApplicationMailer
   default from: 'no-reply@teach-peach.fr'
 
   def welcome_email(user)
-    #on récupère l'instance user pour ensuite pouvoir la passer à la view en @user
     @user = user
-
-    #on définit une variable @url qu'on utilisera dans la view d’e-mail
     @url  = 'http://teach-peach.fr/login'
-
-    # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet.
-    mail(to: @user.email, subject: 'Bienvenue chez nous !')
+    mail(to: @user.email, subject: 'Bienvenue sur Teach Peach')
   end
 end
