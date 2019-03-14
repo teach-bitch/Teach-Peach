92980acc2f49bbda2439a75bb3a8bb305dc8b934
Yacinth <fevriercjjc@gmail.com>
2019-03-15 00:01:34 +0100

    add typeform table

---
+++ db/migrate/20190314172702_create_typeforms.rb
@@ -0,0 +1,12 @@
+class CreateTypeforms < ActiveRecord::Migration[5.2]
+  def change
+    create_table :typeforms do |t|
+      t.belongs_to :user, foreign_key: true
+      t.string :title
+      t.text :description
+      t.string :id_typeform
+
+      t.timestamps
+    end
+  end
+end
