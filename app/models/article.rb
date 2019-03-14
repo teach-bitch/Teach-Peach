c5e11647bed5c13419fa88b45801c7754e0b06e6
Octopussyzz <spudd.otw@gmail.com>
2019-03-14 22:53:42 +0100

    Add validates and error messages

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,12 @@ class Article < ApplicationRecord
   has_many :article_categories, dependent: :nullify
   has_many :categories, through: :article_categories
 
+<<<<<<< HEAD
+=======
+  validates :title,
+		presence: { message: "Tu as oublié le titre de ton article !" }
+
+  validates :content,
+		presence: { message: "Tu as oublié le contenu de ton article !" }
+>>>>>>> Add validates and error messages
 end
