6a49de18a01537920833ba12b72d01ab1c25f36a
Octopussyzz <spudd.otw@gmail.com>
2019-03-15 17:40:47 +0100

    add validates to models

--- app/models/typeform.rb
+++ app/models/typeform.rb
@@ -1,2 +1,14 @@
 class Typeform < ApplicationRecord
+
+  validates :title,
+		presence: { message: "Tu as oublié le titre du formulaire !" }
+
+  validates :description,
+    presence: { message: "Tu as oublié la description du formulaire !" }
+
+  validates :id_typeform,
+    presence: { message: "Tu as oublié de mettre le lien vers le formulaire !" }
+
+  validates :id_results,
+    presence: { message: "Tu as oublié de mettre le lien vers les résultats du formulaire !" }
 end
