b6c56fb073496037db4725f43c8ea3d9b355fe18
Yacinth <fevriercjjc@gmail.com>
2019-03-15 17:16:05 +0100

    add policy for subscription controller

---
+++ app/policies/subscription_policy.rb
@@ -0,0 +1,19 @@
+class SubscriptionPolicy < ApplicationPolicy
+  attr_reader :user, :subscription
+
+  def initialize(user, subscription)
+    @user = user
+    @subscription = subscription
+  end
+  
+  def new?
+    user.role != 'user_minor'
+    
+  end
+
+  def create?
+    user.role != 'user_minor'
+    
+  end
+
+end
