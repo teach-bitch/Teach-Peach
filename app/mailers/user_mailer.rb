55cffe68765e24159015068240283445b6b28ec8
TheWalkingNev <alexandre.gasior@gmail.com>
2019-03-17 12:09:33 +0100

    function to send email to all users we have to remind about sub - DONE

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,8 +7,19 @@ class UserMailer < ApplicationMailer
     mail(to: @user.email, subject: 'Bienvenue sur Teach Peach')
   end
 
-  def subscribe_email(user)
-    @user = user
-    mail(to: @user.email, subject: 'As-tu vu les abonnements ?')
+  def subscribe_email
+    @users = User.all
+    users_to_remind_about_sub = []
+    # Find all users whom are not minor nor subscriber and add them to an array
+    @users.each do |user|
+      if (user.role != 'user_minor') && (user.subscription_id == nil)
+        users_to_remind_about_sub << user
+      end
+    end
+    # Send an email to each users not minor nor subscriber
+    mail(
+      bcc: users_to_remind_about_sub.map(&:email).uniq,
+      subject: "As-tu vu nos abonnements ?"
+    )
   end
 end
