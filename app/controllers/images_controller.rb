5aade7db2bf039efd40aade0ffbbaa3272db3bdf
TheWalkingNev <alexandre.gasior@gmail.com>
2019-03-18 21:47:58 +0100

    def create and update works and redirect to admin page

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,8 +1,5 @@
 class ImagesController < ApplicationController
   def create
-    puts '$' * 50
-    puts 'ICI LA DEF CREATE DU IMAGESCONTROLLER'
-    puts '$' * 50
     @article = Article.find(params[:article_id])
     @article.image.attach(params[:image])
     redirect_to(admins_article_path(@article))
