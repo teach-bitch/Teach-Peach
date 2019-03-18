8eee42df36ab2fefcd27e2ca6c3302de40b092f3
TheWalkingNev <alexandre.gasior@gmail.com>
2019-03-18 22:18:42 +0100

    modified attribut :image to :images

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -64,7 +64,7 @@ class ArticlesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def article_params
-      params.require(:article).permit(:title, :content, :for_adult, :role, :image)
+      params.require(:article).permit(:title, :content, :for_adult, :role, :images)
     end
 
 end
--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,7 +1,7 @@
 class ImagesController < ApplicationController
   def create
     @article = Article.find(params[:article_id])
-    @article.image.attach(params[:image])
+    @article.image.attach(params[:images])
     redirect_to(admins_article_path(@article))
   end
 end
