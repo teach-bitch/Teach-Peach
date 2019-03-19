da6c8114ab695d7dc7ea9aeb5e65350206089b38
TheWalkingNev <alexandre.gasior@gmail.com>
2019-03-19 08:51:35 +0100

    modified controller to allow has_many_attached :image to an article

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,7 +1,7 @@
 class ImagesController < ApplicationController
   def create
     @article = Article.find(params[:article_id])
-    @article.image.attach(params[:images])
+    @article.images.attach(params[:images])
     redirect_to(admins_article_path(@article))
   end
 end
