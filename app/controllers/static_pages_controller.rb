3493d72b6b0f8f260092de4dfcbd62a6bf7f4f16
Yacinth <fevriercjjc@gmail.com>
2019-03-19 20:27:36 +0100

    add regex on content article

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,6 +4,11 @@ class StaticPagesController < ApplicationController
   def home
     @articles = Article.all
     @typeforms = Typeform.all
+    @second_article_text_raw = Article.last(2).first.content.gsub(/<[^>]*>/, '') 
+    @second_article_text_raw_light = @second_article_text_raw.gsub(/&nbsp;/, ' ') 
+
+    @last_article_text_raw = Article.last.content.gsub(/<[^>]*>/, '') 
+    @last_article_text_raw_light = @last_article_text_raw.gsub(/&nbsp;/, ' ') 
   end
 
     private
