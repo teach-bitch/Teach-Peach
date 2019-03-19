0b380d3a312e6f4c387aad201fccc909c297d07c
kokogakayui <kokogakayui@gmail.com>
2019-03-19 09:41:30 +0100

    fix devise login problem after shop integration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
   include Pundit
+  protect_from_forgery
   include CurrentBasket
   before_action :current_basket
-  protect_from_forgery
   before_action :configure_permitted_parameters, if: :devise_controller?
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
