19c0a6ca12240728299ca5d240aa0ff869210b5f
kokogakayui <kokogakayui@gmail.com>
2019-03-19 09:53:02 +0100

    added protect from forgery to config and removed in app controller

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,6 +59,9 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
+  #protect from forgery
+  config.action_controller.default_protect_from_forgery = true
+
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
