731a2bd5c7a334a5fe1c86daa042127b6bc5402e
Octopussyzz <spudd.otw@gmail.com>
2019-03-19 23:17:54 +0100

    Root on admin shop + hover on categories

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ end
     resources :users
     resources :typeforms
     namespace :shop do
+      root "products#index"
       resources :facturations
       resources :join_product_categories
       resources :product_categories
