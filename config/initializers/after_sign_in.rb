9ee81b282674d5efa3cea28ca2845beaf16b2d00
kokogakayui <kokogakayui@gmail.com>
2019-03-20 19:21:32 +0100

    add method to check if user is still minor

--- config/initializers/after_sign_in.rb
+++ config/initializers/after_sign_in.rb
@@ -1,3 +1,5 @@
+# Check if user is still minor when log in
+
 Warden::Manager.after_authentication do |user, auth, opts|
   if user.role == "user_minor"
     if ((Time.zone.now - user.birthdate.to_time) / 1.year.seconds).floor < 18
