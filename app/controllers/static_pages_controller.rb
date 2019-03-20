303f6193b44469fedcd36be1a9f4316e0b7db377
Yacinth <fevriercjjc@gmail.com>
2019-03-20 15:22:25 +0100

    clean static pages controller

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,14 +1,12 @@
 class StaticPagesController < ApplicationController
   before_action :set_user
+  include StaticPagesHelper
+
   
   def home
     @articles = Article.all
     @typeforms = Typeform.all
-    @second_article_text_raw = Article.last(2).first.content.gsub(/<[^>]*>/, '') 
-    @second_article_text_raw_light = @second_article_text_raw.gsub(/&nbsp;/, ' ') 
-
-    @last_article_text_raw = Article.last.content.gsub(/<[^>]*>/, '') 
-    @last_article_text_raw_light = @last_article_text_raw.gsub(/&nbsp;/, ' ') 
+    article_clean
   end
 
     private
