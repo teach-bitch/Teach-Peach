a8d8ee7eeb883f12e381460e60579c90a02eafcf
Yacinth <fevriercjjc@gmail.com>
2019-03-20 22:11:57 +0100

    move method in articles helper

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,11 +1,4 @@
 module StaticPagesHelper
 
-  def article_clean
-    @second_article_text_raw = Article.last(2).first.content.gsub(/<[^>]*>/, '')
-    @second_article_text_raw_light = @second_article_text_raw.gsub(/&nbsp;/, ' ')
-
-    @last_article_text_raw = Article.last.content.gsub(/<[^>]*>/, '')
-    @last_article_text_raw_light = @last_article_text_raw.gsub(/&nbsp;/, ' ')
-  end
   
 end
