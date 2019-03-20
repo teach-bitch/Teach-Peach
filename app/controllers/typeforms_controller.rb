ba2ad74e25f6eb2db4118a91d7079df5bee4b90c
Yacinth <fevriercjjc@gmail.com>
2019-03-21 00:56:24 +0100

    clean controller

--- app/controllers/typeforms_controller.rb
+++ app/controllers/typeforms_controller.rb
@@ -1,24 +1,15 @@
 class TypeformsController < ApplicationController
   before_action :set_typeform, only: [:show]
 
-  # GET /typeforms/1
-  # GET /typeforms/1.json
-def index
-   @typeforms = Typeform.all
-   
-end
-
   def show
     @typeform_id = Typeform.last.id_typeform
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_typeform
       @typeform = Typeform.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def typeform_params
       params.require(:typeform).permit(:title, :description, :id_typeform, :id_results)
     end
