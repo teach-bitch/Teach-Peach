8bfa0c8e41b1141c89213f39d1e64b986d5e0a9f
willyspa <armed95@hotmail.fr>
2019-03-21 12:01:00 +0100

    add factory bot join_product_categories

--- spec/factories/join_product_categories.rb
+++ spec/factories/join_product_categories.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :join_product_category do
-    
+    product_id { FactoryBot.create(:product).id }
+    product_category_id { FactoryBot.create(:product_category).id }
   end
 end
