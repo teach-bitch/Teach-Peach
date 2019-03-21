98cd9db99bfd087535a376ef3bd41b1725966522
willyspa <armed95@hotmail.fr>
2019-03-21 20:17:00 +0100

    fixing model product validates

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,15 +5,15 @@ class Product < ApplicationRecord
   has_many :product_categories, through: :join_product_categories
 
   validates :name,
-    presence: { message: "Tu as oublié de spécifier le nom du produit !" }
+    presence: { message: "Tu as oublié de spécifier le nom du produit !" },
     length: { in: 3..140 }
 
   validates :price,
-    presence: { message: "Tu as oublié de spécifier le prix!" }
+    presence: { message: "Tu as oublié de spécifier le prix!" },
     numericality: { greater_than: 0, less_than: 1001 }
 
   validates :description,
-    presence: { message: "Tu as oublié de spécifier une description !" }
+    presence: { message: "Tu as oublié de spécifier une description !" },
     length: { in: 10..1000 }
 
 end
