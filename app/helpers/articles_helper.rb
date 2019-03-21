709e5d4fd07b5097d976b597f71ae1be7722cc05
kokogakayui <kokogakayui@gmail.com>
2019-03-21 16:19:10 +0100

    fix helper articles

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,29 +1,30 @@
 module ArticlesHelper
 
   def set_articles
-    
-    if !user_signed_in? || current_user.role == 'user_minor'
-      @articles = []
-      Article.all.each do |article|
-        if article.for_adult == false
-          @articles << article
+    if @articles != nil
+      if !user_signed_in? || current_user.role == 'user_minor'
+        @articles = []
+        Article.all.each do |article|
+          if article.for_adult == false
+            @articles << article
+          end
         end
+      else
+        @articles = Article.all
       end
-    else
-      @articles = Article.all
     end
   end
 
   def article_clean
-    
+
     if @articles != nil
-      
+
       @second_article_text_raw = @articles.last(2).first.content.gsub(/<[^>]*>/, '')
       @second_article_text_raw_light = @second_article_text_raw.gsub(/&nbsp;/, ' ')
-  
+
       @last_article_text_raw = @articles.last.content.gsub(/<[^>]*>/, '')
       @last_article_text_raw_light = @last_article_text_raw.gsub(/&nbsp;/, ' ')
-      
+
     end
   end
 
