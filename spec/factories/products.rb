be1c0e029c009dbae77019aba2fab468ab09cb4a
willyspa <armed95@hotmail.fr>
2019-03-21 21:15:24 +0100

    fix factory products

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -3,8 +3,8 @@ FactoryBot.define do
     name { Faker::FunnyName.name }
     description { Faker::GreekPhilosophers.quote }
     brand { Faker::Company.name }
-    price { Faker::Number.decimal(2) }
-    quantity { Faker::Number.number(2) }
+    price { Faker::Number.decimal(2).to_f }
+    quantity { Faker::Number.number(2).to_i }
     reference { Faker::IDNumber.brazilian_citizen_number }
   end
 end
