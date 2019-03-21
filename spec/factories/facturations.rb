21eaa1ef7c600f32c943c783f3239d1a6417e955
willyspa <armed95@hotmail.fr>
2019-03-21 12:01:00 +0100

    add factory bot join_product_categories

--- spec/factories/facturations.rb
+++ spec/factories/facturations.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :facturation do
     price { Faker::Number.decimal(2) }
+    basket_id { FactoryBot.create(:basket).id }
+    user_id { FactoryBot.create(:user).id }
   end
 end
