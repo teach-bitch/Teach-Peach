a8e49bf4c3c3513cee06006ce251357de38a0678
TheWalkingNev <alexandre.gasior@gmail.com>
2019-03-21 22:38:26 +0100

    spec for line item done

---
+++ spec/models/line_item_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.describe LineItem, type: :model do
+
+  before(:each) do
+    @line_item = FactoryBot.create(:line_item)
+  end
+
+  it "has a valid factory" do
+    expect(build(:user)).to be_valid
+  end
+
+  context "validation" do
+
+    it "is valid with valid attributes" do
+      expect(@line_item).to be_a(LineItem)
+    end
+
+  end
+
+  context "associations" do
+    describe "line_items" do
+      it { expect(@line_item).to belong_to(:basket) }
+      it { expect(@line_item).to belong_to(:product) }
+    end
+  end
+
+  context "class" do
+    it "should return a string" do
+      expect(@line_item.quantity).to be_a(Integer)
+    end
+  end
+
+end
