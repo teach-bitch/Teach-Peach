1672879af7e58cdc6ee5eea66ccf5abe06fd6353
smelldat <valentin.cluzan33@gmail.com>
2019-03-22 17:59:43 +0100

    no access to the shop for minors

--- app/controllers/shop_controller.rb
+++ app/controllers/shop_controller.rb
@@ -1,3 +1,12 @@
 class ShopController < ApplicationController
   before_action :authenticate_user!
+  before_action :redirect_if_is_user_minor
+
+  private
+
+  def redirect_if_is_user_minor
+  	if current_user.role == "user_minor"
+  		redirect_to root_path
+  	end
+  end
 end
